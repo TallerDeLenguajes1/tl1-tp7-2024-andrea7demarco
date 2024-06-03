e731fd91b82eddb3fa285bebb6675d7bb57cf02f
Andrea Sofia De Marco <andreasofiademarco@gmail.com>
2024-06-03 12:36:20 -0300

    p1 finalizado

---
+++ Calculadora.cs
@@ -0,0 +1,52 @@
+﻿using System.Formats.Asn1;
+using EspacioCalculadora;
+string? input;
+bool parseoBool1;
+int opcion = 0;
+Calculadora cal=new Calculadora();  //cal objeto q yo quiero crear  
+Console.WriteLine("Numero :");
+input = Console.ReadLine();
+parseoBool1 = float.TryParse(input, out float numero);
+do {
+  Console.WriteLine("Elija la operacion a realizar:\n");
+  Console.WriteLine("1. Suma\n");
+  Console.WriteLine("2. Resta\n");
+  Console.WriteLine("3. Multiplicacion\n");
+  Console.WriteLine("4. Division\n");
+  Console.WriteLine("5. Limpiar\n");
+  Console.WriteLine("6. Salir\n");
+
+  if(parseoBool1)
+  {
+   Console.WriteLine("Ingrese la operacion:\n");
+   input = Console.ReadLine();
+   parseoBool1 = int.TryParse(input, out opcion);
+   
+   if(opcion <6 && opcion >0)
+   {
+    switch(opcion)
+    {
+      case 1: 
+          cal.Suma(numero);
+          Console.WriteLine(cal.Resultado);
+          break;
+      case 2: 
+          cal.Resta(numero);
+          Console.WriteLine(cal.Resultado);
+          break;
+      case 3:
+          cal.Multiplicacion(numero);
+          Console.WriteLine(cal.Resultado);
+          break;
+      case 4:
+          cal.Division(numero);
+          Console.WriteLine(cal.Resultado);
+          break;
+      default:
+          Console.WriteLine("Opcion no valida");
+          break;
+    }
+   }
+
+  }
+}while(opcion != 6);
--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace EspacioCalculadora;
+
+public class Calculadora{ 
+    private double valor; //atributo privado 
+
+    public Calculadora() //constructor 
+    {
+        valor=0;
+    }
+
+    public double Resultado { get => valor;} //prop ,definir get
+
+
+    public void Suma(double sumando)   //metodo
+    {
+        valor+=sumando;
+    }
+
+    public void Resta(double sustraendo)
+    {
+        valor-=sustraendo;
+    }
+
+    public void Multiplicacion(double factor)
+    {
+        valor*=factor;
+    }
+
+    public void Division(double divisor)
+    {   
+        valor/=divisor;
+    }
+
+    public void Limpiar()
+    {
+        valor=0;
+    }
+
+
+
+
+
+
+}
