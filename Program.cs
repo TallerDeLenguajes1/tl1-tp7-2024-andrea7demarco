36a14e5653ed12fb70031f6f39daed51e8201c23
Andrea Sofia De Marco <andreasofiademarco@gmail.com>
2024-07-12 11:23:10 -0300

    Terminado

--- Program.cs
+++ Program.cs
@@ -1,44 +1,58 @@
-﻿namespace EspacioCalculadora;
+﻿using Espacio.Empleados;
 
-public class Calculadora{ 
-    private double valor; //atributo privado 
+Empleado[] empleados = new Empleado[3];
 
-    public Calculadora() //constructor 
-    {
-        valor=0;
+void empleadoProximoJubilarse(Empleado[] empleados){
+    int j = 0;
+    int proximo = 9999;
+    for(int i = 0; i < empleados.Count(); i++){
+    if(empleados[i].Jubilacion() <= proximo){
+        proximo = empleados[i].Jubilacion();
+        j = i;
+        }
     }
-
-    public double Resultado { get => valor;} //prop ,definir get
-
-
-    public void Suma(double sumando)   //metodo
-    {
-        valor+=sumando;
-    }
-
-    public void Resta(double sustraendo)
-    {
-        valor-=sustraendo;
-    }
-
-    public void Multiplicacion(double factor)
-    {
-        valor*=factor;
-    }
-
-    public void Division(double divisor)
-    {   
-        valor/=divisor;
-    }
-
-    public void Limpiar()
-    {
-        valor=0;
-    }
-
-
-
-
-
-
+    empleados[j].MostrarDatos();
 }
+
+double totalSalarios = 0;
+
+//Datos Empleado 1
+empleados[0] = new Empleado();
+empleados[0].Apellido = "Toll";
+empleados[0].Nombre = "Luciana";
+empleados[0].Fecha_nacimiento = new DateTime(2003, 6, 26);
+empleados[0].Estado_civil = 'S';
+empleados[0].Genero = 'F';
+empleados[0].Fecha_ingreso = new DateTime(2020, 5, 20);
+empleados[0].Sueldo_basico = 50000;
+empleados[0].Cargo = Cargos.Ingeniero;
+
+//Datos Empleado 2
+empleados[1] = new Empleado();
+empleados[1].Apellido = "Barbosa";
+empleados[1].Nombre = "Sandra";
+empleados[1].Fecha_nacimiento = new DateTime(2002, 2, 1);
+empleados[1].Estado_civil = 'C';
+empleados[1].Genero = 'F';
+empleados[1].Fecha_ingreso = new DateTime(2015, 10, 10);
+empleados[1].Sueldo_basico = 35000;
+empleados[1].Cargo = Cargos.Investigador;
+
+//Datos Empleado 3
+empleados[2] = new Empleado();
+empleados[2].Apellido = "De La Cruz";
+empleados[2].Nombre = "Federico";
+empleados[2].Fecha_nacimiento = new DateTime(2002, 5, 26);
+empleados[2].Estado_civil = 'S';
+empleados[2].Genero = 'M';
+empleados[2].Fecha_ingreso = new DateTime(2015, 10, 10);
+empleados[2].Sueldo_basico = 40000;
+empleados[2].Cargo = Cargos.administrativo;
+
+foreach(Empleado empleado in empleados){
+    totalSalarios += empleado.Salario();
+}
+
+Console.WriteLine($"Monto Total en Salarios: {totalSalarios}");
+Console.WriteLine($"Empleado Proximo a jubilarse:");
+empleadoProximoJubilarse(empleados);
